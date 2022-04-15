59a484522ea7d54c60a9d71b7f9671916412d8ea
Stephen Li <stephenli.ca@gmail.com>
2022-04-14 21:17:44 -0400

    Disable source map for better dev performance

--- webpack.config.ts
+++ webpack.config.ts
@@ -36,9 +36,6 @@ const config: webpack.Configuration = {
     mode: isDev
         ? 'development'
         : 'production',
-    devtool: isDev
-        ? 'inline-source-map'
-        : false,
 
     entry: path.resolve(srcDir, 'main.ts'),
     output: {
